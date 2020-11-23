dbf52fda227ba849c25d2f5080c78814f834c964
pruthvisoni1604 <48025357+pruthvisoni1604@users.noreply.github.com>
2020-11-23 16:34:28 -0500

    Add project files.

---
+++ EventManagment_console_.net/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagment_console_.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
